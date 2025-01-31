e3c4a762721e5b9e477a8e49cdbb7c61e8f45179
PHM-dung2 <pp4992@naver.com>
2025-01-31 15:34:17 +0900

    jsp day09 파일첨부 - uuid 적용

--- tj2024b_web1/src/main/java/day09/Example4.java
+++ tj2024b_web1/src/main/java/day09/Example4.java
@@ -3,6 +3,7 @@ package day09;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
@@ -36,28 +37,37 @@ public class Example4 extends HttpServlet {
 //			1. DiskFileItemFactory 클래스 이용한 1. 저장위치 2. 업로드 용량제한 3. 한글 인코딩 설정
 		DiskFileItemFactory factory = new DiskFileItemFactory();
 		factory.setRepository( file ); 				// 1. 저장 위치 설정
-		factory.setSizeThreshold( 1024 * 1024 );	// 2. 업로드 용량 제한 설정 , 1바이트 기준 , 1024바이트 -> 1kb , 1024*1024 -> 
+		factory.setSizeThreshold( 1024 * 1024 );	// 2. 업로드 용량 제한 설정 , 1바이트 기준 , 1024바이트 -> 1kb , 1024*1024 -> 1mb
 		factory.setDefaultCharset("UTF-8");			// 3. 한글 인코딩 설정
 //			2. ServletFileUpload 클래스에 위에서 설정한 기준으로 업로드 객체 생성한다.
 		ServletFileUpload fileUpload = new ServletFileUpload( factory );
 //			3. 업로드 객체에 HTTP 요청 객체를 대입하여 HTTP 요청객체를 업로드객체로 변환하고, 반환된 자료들을 리스트로 받는다.
 		try {
-		List< FileItem > formItems = fileUpload.parseRequest( req ); 
-		//		오류발생 이유 : <-- cos.jar / commons.jar 톰캣 10.1버전을 지원하지 않는다. 톰캣 9 이하 설치 또는 마이그레이션 이용하여 라이브러리 버전 낮추기
-		//		4. 만약에 업로드 객체 내 반환된 자료들이 존재하면
+			List< FileItem > formItems = fileUpload.parseRequest( req ); 
+//				오류발생 이유 : <-- cos.jar / commons.jar 톰캣 10.1버전을 지원하지 않는다. 톰캣 9 이하 설치 또는 마이그레이션 이용하여 라이브러리 버전 낮추기
+//				4. 만약에 업로드 객체 내 반환된 자료들이 존재하면
 			if( formItems != null && !formItems.isEmpty() ) {
-		//		5. 반복문을 이용한 form 자료들을 하나씩 확인한다.
+//				5. 반복문을 이용한 form 자료들을 하나씩 확인한다.
 				for( int index = 0 ; index <= formItems.size()-1; index++ ) {
-		//			6. 자료 1개만 추출
+//					6. 자료 1개만 추출
 					FileItem fileItem = formItems.get(index);
-		//			7. 자료가 첨부파일(파일)인지 단순한 문자열인지 구분
+//					7. 자료가 첨부파일(파일)인지 단순한 문자열인지 구분
 					if( fileItem.isFormField() ) { // 일반 폼 자료인지
 						System.out.println("첨부파일이 아닌 일반 텍스트 : " + fileItem.getName() );
 					}else { // 첨부파일 폼 자료인지
 						System.out.println("첨부파일 : " +  fileItem.getName() );
-		//				8. 만일 첨부파일이면 현재 업로드 경로에 파일명 붙이기
-						File uploadFile = new File( uploadPath + "/" + fileItem.getName() );
-		//				9. 지정한 파일명으로 업로드 처리
+//							* 만일 서로 다른 사람이 동일한 파일명을 올릴 경우 식별이 불가능. --> 방안 : UUID 내장함수
+//								- UUID는 16진수로 5자리의 임의 자료 생성한다.
+							String uuid = UUID.randomUUID().toString();
+							System.out.println( uuid ); // afdbd2bb-81bf-41f4-9bdd-198617d1d7c8 (실행마다 다르게 발급)
+//								- UUID와 업로드 파일명 연결 , 만약에 파일명에 '-'(하이픈) 포함되면 uuid와 파일명 구분이 힘들다.
+//								만약에 사용자가 업로드한 파일명에 -(하이픈)을 _(언더바)로 변경하기. 이유: -(하이픈)은 uuid 구분용
+//							"문자열".replaceAll( "변경할문자" , "새로운문자" ); : 문자열 내 변경할 문자가 존재하면 새로운 문자로 치환
+//							예시) afdbd2bb-81bf-41f4-9bdd-198617d1d7c8-짱구.jpg
+							String filename = uuid +"-"+ fileItem.getName().replaceAll("-", "_");
+//						8. 만일 첨부파일이면 현재 업로드 경로에 파일명 붙이기
+							File uploadFile = new File( uploadPath + "/"+filename );
+//						9. 지정한 파일명으로 업로드 처리
 						fileItem.write(uploadFile);
 					}
 				} // for end
